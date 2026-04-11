14a7a31229a42b2a5726032b8d5b6d1bbc152b2a
eyescosmos <analogriders@gmail.com>
2026-04-11 18:27:24 +0900

    Refine mobile hamburger icon styling

--- scripts/generate_photographer_pages.py
+++ scripts/generate_photographer_pages.py
@@ -13,7 +13,7 @@ from urllib.parse import urlparse
 REPO = Path("/Users/aiharadaisuke/Documents/New project/repo")
 SITE = "https://eyescosmos.github.io"
 GA_ID = "G-2VRTV8BZEJ"
-ASSET_VERSION = "20260411e"
+ASSET_VERSION = "20260411f"
 ALNUM_BOUNDARY_RE = re.compile(r"[A-Za-z0-9]")
 NON_PHOTOGRAPHER_IDS = {
     "anri-sala",
