3b062aa2c5f5d98455c0526bc2672144892611c3
eyescosmos <analogriders@gmail.com>
2026-04-17 21:07:41 +0900

    Refine mobile nav layout and top page links

--- scripts/generate_taxonomy_pages.py
+++ scripts/generate_taxonomy_pages.py
@@ -11,7 +11,7 @@ import re
 REPO = Path("/Users/aiharadaisuke/Documents/New project/repo")
 SITE = "https://eyescosmos.github.io"
 GA_ID = "G-2VRTV8BZEJ"
-ASSET_VERSION = "20260417e"
+ASSET_VERSION = "20260417g"
 NON_PHOTOGRAPHER_IDS = {
     "anri-sala",
     "ana-torfs",
